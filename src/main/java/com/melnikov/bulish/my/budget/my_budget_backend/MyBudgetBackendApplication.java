94f6fcb96cbc7059f75d1fd53c48cf7ab8358dd9
Evgenia Bullis <bulish2015@yandex.ru>
2024-08-01 17:50:57 +0300

    initial commit

---
+++ src/main/java/com/melnikov/bulish/my/budget/my_budget_backend/MyBudgetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.melnikov.bulish.my.budget.my_budget_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBudgetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBudgetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melnikov/bulish/my/budget/my_budget_backend/MyBudgetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melnikov.bulish.my.budget.my_budget_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBudgetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
